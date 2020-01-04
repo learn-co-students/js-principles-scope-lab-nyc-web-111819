ed9097116ddae7ebbad0905f317ffe34ae6e5173
Rines.io <tsrines@gmail.com>
2020-01-04 10:27:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'Joe the Constant'
+var bestCustomer;
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'steve'
+}
+
